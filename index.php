971d57953fdfbb9eefffc3cbcf0d82b08730df92
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-11-15 14:46:41 +0100

    php exo8

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP</title>
+</head>
+<body>
+  <?php
+$a = 3 + 4;
+$b = 5 * 20;
+$c = 45 / 5;
+settype($a,"int");
+settype($b,"int");
+settype($c,"int");
+echo $a . " " . $b . " " . $c ;
+?>
+</body>
+</html>
